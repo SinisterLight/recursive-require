00d507cfe76a3301a2eac0121cd1db93fda87be3
Sinister Light <me@SinisterLight.com>
2013-06-10 14:41:14 +0530

    [Shobhit] setting up mocha

---
+++ test/main.js
@@ -0,0 +1,8 @@
+var should  = require('should');
+
+describe('recusive-reqiure', function(){
+  it('should require js in a folder path and map it to object with attribute with file name');
+  it('should ignore non-js file');
+  it('should require js in sub folders and create an object tree for files');
+});
+
