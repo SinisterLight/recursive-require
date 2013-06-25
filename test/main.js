1ccb9814d2699a7cb175960c3020361e2817a301
Sinister Light <me@SinisterLight.com>
2013-06-25 15:17:13 +0530

    [Shobhit] deleting unnecessary files

--- test/main.js
+++
@@ -1,8 +0,0 @@
-var should  = require('should');
-
-describe('recusive-reqiure', function(){
-  it('should require js in a folder path and map it to object with attribute with file name');
-  it('should ignore non-js file');
-  it('should require js in sub folders and create an object tree for files');
-});
-
